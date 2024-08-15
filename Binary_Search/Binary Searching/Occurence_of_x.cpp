b2402cbddef1bd41f9772006c77748b2a507827d
Subhanulhaq935 <164912119+Subhanulhaq935@users.noreply.github.com>
2024-08-15 11:16:27 -0700

    Update Occurence_of_x.cpp

--- Binary_Search/Binary Searching/Occurence_of_x.cpp
+++ Binary_Search/Binary Searching/Occurence_of_x.cpp
@@ -1,35 +1,84 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int count(int arr[], int n, int x)
+int first_position(const vector<int> &arr, int n, int x)
 {
-    int i = 0;
-    int count = 0;
-    while(i < n)
-    {
-        if(arr[i] == x)
-        {
-            while(arr[i] == x)
-            {
-                count++;
-                i++;
-            }
+    int l = 0;
+    int h = n - 1;
+    int first = -1;
 
-            break;
+    while (l <= h)
+    {
+        int mid = (l + h) / 2;
+
+        if (arr[mid] == x)
+        {
+            first = mid;
+            h = mid - 1;
+        }
+
+        else if (arr[mid] < x)
+        {
+            l = mid + 1;
         }
         else
         {
-            i++;
+            h = mid - 1;
         }
     }
-    return count;
+
+    return first;
+}
+
+int last_position(const vector<int> &arr, int n, int x)
+{
+    int l = 0;
+    int h = n - 1;
+    int last = -1;
+
+    while (l <= h)
+    {
+        int mid = (l + h) / 2;
+
+        if (arr[mid] == x)
+        {
+            last = mid;
+            l = mid + 1;
+        }
+
+        else if (arr[mid] < x)
+        {
+            l = mid + 1;
+        }
+        else
+        {
+            h = mid - 1;
+        }
+    }
+
+    return last;
+}
+
+int count_Occurances(const vector<int> arr, int n, int x)
+{
+    int first = first_position(arr, n, x);
+    int last = last_position(arr, n, x);
+    int count = last - first + 1;
 }
 
 int main()
 {
-    int arr[7] = {1, 1, 2, 2, 2, 2, 3};
-    int c = count(arr, 7, 5);
-    cout << c;
+    vector<int> arr = {1, 2, 3, 6, 6, 6, 6};
+    int n = arr.size();
+    int x = 4;
+
+    int first = first_position(arr, n, 6);
+    int last = last_position(arr, n, 6);
+
+    cout << "First Position is: " << first << " " << "and " << "Lasۨt position is: " << last << endl;
+    int c = count_Occurances(arr, n, 6);
+    cout << "Occurances of 6 is: " << c;
 
     return 0;
 }
